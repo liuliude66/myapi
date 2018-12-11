a1481fe38448f10010607bfa599ffe80c36ebed5
liuliude66 <liu0000006@gmail.com>
2018-12-11 20:43:38 +0800

    【Init】 — 初始化 投注记录查询接口

--- com/forum/lottery/main.py
+++ com/forum/lottery/main.py
@@ -4,18 +4,22 @@
 from com.forum.lottery.api.LoginApi import LoginApi
 from com.forum.lottery.api.CountdownApi import CountdownApi
 from com.forum.lottery.api.LotteryApi import LotteryApi
+from com.forum.lottery.api.BetRecordApi import BetRecordApi
 
 if __name__ == '__main__':
 
     print("------->  start  <-------")
+    # 登录接口
     username = '1002'
     password = '123456'
-    loginApiInstance = LoginApi(username, password)
-    loginApiInstance.run()
+    LoginApi(username, password).run()
 
-    countdownInstance = CountdownApi()
-    countdownInstance.run()
+    # 倒计时接口
+    CountdownApi().run()
 
-    lotteryInstance = LotteryApi()
-    lotteryInstance.run()
+    # 开奖接口
+    LotteryApi().run()
+
+    #投注记录查询接口
+    BetRecordApi().run()
     print("------->  over  <-------")
