7b3d27375e54fc9ded05295d3b7869f92e460e00
liuliude66 <liu0000006@gmail.com>
2018-12-19 21:17:58 +0800

    【Improve】 — 完善 添加一点注释

--- com/forum/lottery/config/GlobalParams.py
+++ com/forum/lottery/config/GlobalParams.py
@@ -6,10 +6,10 @@ from rootPath import getRootPath
 GlobalConfig = {
     'MAX_RETRY': 3,  # 请求错误最大重试次数
     'REQUEST_TIMEOUT': 15,  # 网络超时请求时间
-    'DOMAIN': 'http://5550987.com',  # 网络请求域名
+    'DOMAIN': 'http://msg2.0234.co',  # 网络请求域名
     'Client_version': "5.2.3", #当前版本号
-    'username': "1002", #测试账号
-    'password': "123456", #测试密码
+    'username': "1002", #投注测试账号
+    'password': "123456", #投注测试密码
     'SESSION_ID': '',  # sessionid 请求头中
     'USER_ID': '',  # userid 可能用做参数
     'SUCCESS_COUNT': 0,  # 成功的个数
